f881d20fe0d67a8977af094b1f037058cedc5aca
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-17 12:21:09 +0800

    вынесен sidebar

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -40,13 +40,7 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Sidebar -->
-            <div class="sidebar">
-
-            </div>
-            <!-- /.sidebar -->
-        </aside>
+        @include('admin.includes.sidebar')
 
         <!-- Content Wrapper. Contains page content -->
         @yield('content')
