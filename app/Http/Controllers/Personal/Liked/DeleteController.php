fe5388273407c5d37fddf3e2089c8c259fbcdf93
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-17 23:06:00 +0800

    Коментарии

--- app/Http/Controllers/Personal/Liked/DeleteController.php
+++ app/Http/Controllers/Personal/Liked/DeleteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Personal\Liked;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 
-class IndexController extends Controller
+class DeleteController extends Controller
 {
     public function __invoke(Post $post)
     {
--- resources/views/personal/comment/index.blade.php
+++ resources/views/personal/comment/index.blade.php
@@ -23,6 +23,41 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                        <th colspan="2">Действие</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($comments as $comment)
+                                    <tr>
+                                        <th>{{ $comment->id }}</th>
+                                        <th>{{ $comment->message }}</th>
+                                        <th><a href="{{ route('personal.comment.edit', $comment->id)}}"><i class="fas fa-pencil-alt"></i></a></th>
+
+                                        <th>
+                                            <form action="{{ route('personal.comment.delete', $comment->id)}}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i class="fas fa-trash text-danger" role="button"></i>
+                                                </button>
+                                            </form>
+                                        </th>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- /.row -->
         </div><!-- /.container-fluid -->
